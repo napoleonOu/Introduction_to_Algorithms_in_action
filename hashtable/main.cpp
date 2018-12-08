68aa3fa58101358880c68715941ca7b09cba1570
wangmin <wangminsuccess@gmail.com>
2018-12-08 22:51:19 +0800

    hashtable

--- hashtable/main.cpp
+++ hashtable/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include "hashtable.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    /*
+    hashtable<int,int,hash<int>,identity<int>,equal_to<int>>
+            iht(50,hash<int>(),equal_to<int>());
+
+    */
+    HashTable<int,int> ht(50);
+    for(int i=0;i!=50;i++){
+        std::string tmp="aaa";
+        auto p1=std::make_pair(i,i+1000);
+        ht.Insert(p1);
+    }
+    std::cout<<(ht.Find(8))->_kv.second;
+    ;
+
+
+
+    //cout<<"bucket count = "<<iht.bucket_count()<<endl;
     return 0;
 }
-
